fcb13a983959b12d1fea74b941a24aa5349368fd
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2015-11-22 23:38:23 +0100

    Dodaje zadanie gulp do tworzenia tabeli i indeksów w bazie rethinkdb

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,12 @@
 'use strict';
 var gulp = require('gulp');
 
-require('./gulp/app');
-require('./gulp/watch');
+require('./gulp/app')
+require('./gulp/watch')
+require('./gulp/rethinkdb')
 
 gulp.task('build', [ 'app' ]);
 
-gulp.task('default', ['build'], function() {
+gulp.task('default', ['rethinkdb', 'build'], function() {
   return gulp.start('serve', 'watch');
 });
