d98878e88c3ce9966a077eea89c779dc8ce7e91d
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2015-12-20 23:22:53 +0100

    [API] Dodaje wstępną dokumentację
    Opis procesu logowania OAuth 2.0.

--- api.js
+++ api.js
@@ -5,13 +5,20 @@ var express = require('express')
 var passport = require('passport')
 var util = require('util')
 var bodyParser = require('body-parser')
-var session = require('express-session')
+var expressSession = require('express-session')
 
 var config = require('./config.json')
 var oauth2 = require('./oauth/oauth2')
 
+var Volonteer = require('./app/services/'+config.service+'/volonteers')
+
 // Express configuration
 
+var session = expressSession({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+})
 var server = express();
 
 server.set('view engine', 'ejs');
@@ -20,7 +27,6 @@ server.set('views', process.cwd() + '/oauth/views')
 //server.use(express.cookieParser());
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
-server.use(session({ secret: 'keyboard cat' }))
 /*
 server.use(function(req, res, next) {
   console.log('-- session --');
@@ -42,31 +48,67 @@ require('./oauth/auth');
 
 // Formularz do logowania dla wolontariuszy chcących dać dostęp do swojego
 // konta wybranej aplikacji.
-server.get('/login', function(req, res) { res.render('login') })
-server.post('/login', passport.authenticate('local', {
+server.get('/login', session, function(req, res) { res.render('login') })
+server.post('/login', session, passport.authenticate('local', {
   successReturnToOrRedirect: '/',
   failureRedirect: '/login'
 }))
 
-server.get('/logout', function(req, res) {
+server.get('/logout', session, function(req, res) {
   req.logout()
   res.redirect('/')
 })
 
 // Okienko w którym wolontariusz wyraża zgodę (lub nie) na dostęp do swojego
 // konta.
-server.get('/dialog/authorize', oauth2.authorization);
-server.post('/dialog/authorize/decision', oauth2.decision);
+server.get('/dialog/authorize', session, oauth2.authorization);
+server.post('/dialog/authorize/decision', session, oauth2.decision);
 
 // Końcówka dla klienta oauth chcącego zamienić tymczasowy kod dostępu na
 // token.
 server.post('/oauth/token', oauth2.token);
 
-server.get('/client', passport.authenticate('bearer', { session: false }), function(req, res) {
+// Autoryzacja tokenem oAuth
+var bearer = passport.authenticate('bearer', { session: false })
+
+server.get('/client', bearer, function(req, res) {
   res.json({
     client_id: req.user.id,
     scope: req.authInfo.scope
   })
 })
+
+// Lista wolontariuszy
+server.get('/volunteers', bearer, function(req, res) {
+  Volonteer.read(req, 'Volonteers', {}, req.query, function (err, users) {
+    res.send(users)
+  })
+})
+
+// Szczegóły wolontariusza
+server.get('/volunteers/:id', bearer, function(req, res) {
+  Volonteer.read(req, 'Volonteers', {id: req.params.id}, {}, function (err, user) {
+    res.send(user)
+  })
+})
+
+// Aktualizacja wolontariusza
+server.post('/volunteers/:id', bearer, function(req, res) {
+  Volonteer.update(req, 'Volonteers', {id: req.params.id}, req.body, {}, function (err, user) {
+    res.send(user)
+  })
+})
+
+// Lista zadań
+//server.get('/tasks', bearer, function(req, res) { })
+
+// Szczegóły zadania
+//server.get('/tasks/:id', bearer, function(req, res) { })
+
+// Zgłoszenie do zadania
+//server.post('/tasks/:id/join', bearer, function(req, res) { })
+
+// Baza noclegowa pielgrzymów
+//server.get('/pilgrims', bearer, function(req, res) { })
 
 server.listen(config.api_port);
