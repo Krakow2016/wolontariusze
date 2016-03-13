b164713856cad5773bf56785afc716276eca90be
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2016-03-13 23:35:09 +0100

    Przywraca poprzedni plik docker-compose.yml

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 'use strict'
 
 var express = require('express')
-var gutil = require('gulp-util')
 var env = process.env.NODE_ENV || 'development'
 var config = require('./config.json')[env]
 
 var app = express()
 require('./server')(app).listen(config.port)
 
-gutil.log(gutil.colors.bgGreen('Serwer został uruchomiony i jest dostępny pod adresem: http://127.0.0.1:'+config.port+'/.'))
+console.log('Serwer został uruchomiony i jest dostępny pod adresem: http://127.0.0.1:'+config.port+'/.')
