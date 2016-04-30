0aaf41d538aaa8acca323d6a7d371f8e60bd0907
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2016-04-30 18:47:26 +0200

    Poprawia testy

--- spec/api_pilgrims_spec.js
+++ spec/api_pilgrims_spec.js
@@ -2,6 +2,7 @@ describe('Pilgrims API', function() {
   describe('get /pilgrims/', function() {
     it('should return full pilgrims database', function(done) {
       // TODO
+      done()
     })
   })
 })
