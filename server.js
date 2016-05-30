c582677ab4cee5b3e7b96ab1caa945e5765fa6ab
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2016-05-31 00:08:39 +0200

    Dodaje obsługę błędu zmiany zdjęcia profilowego

--- server.js
+++ server.js
@@ -640,7 +640,12 @@ module.exports = function(server) {
             thumb_picture_url: data[2].Location +'?'+ data[1].ETag.replace(reg, '')
           }
           Volunteers.update({force_admin: true}, 'Volunteers', {id: req.user.id}, changes, {returnChanges: true}, function(err, result) {
-            res.status(201).send(result.changes[0].new_val)
+            if(err) {
+              res.status(500).send('Wystąpił nieznany błąd bazy danych.')
+              console.error(err)
+            } else {
+              res.status(201).send(result.changes[0].new_val)
+            }
           })
         }
       })
