f4d94aec3fb48ba8849368cb3ad31f8b55bc4e89
Janek <j.jaracz@poczta.pl>
2016-07-20 23:03:03 +0200

    Poprawka authentication

--- app/components/Authentication.jsx
+++ app/components/Authentication.jsx
@@ -3,15 +3,33 @@ var NavLink = require('fluxible-router').NavLink
 var FormattedMessage = require('react-intl').FormattedMessage
 
 var Authentication = React.createClass({
+
+  getInitialState: function(){
+    var state = {
+      open: false
+    }
+    return state
+  },
+
+  onClicked: function(){
+    this.setState({
+      open: !this.state.open
+    })
+  },
+
   render: function () {
     var loginButton
     var classname
     if (this.props.user_name) {
       loginButton = <LogoutButton
         user_id={this.props.user_id}
-        user_name={this.props.user_name} />
+        user_name={this.props.user_name}
+        open={this.state.open}
+        onClicked={this.onClicked} />
     } else {
-      loginButton = <LoginButton />
+      loginButton = <LoginButton 
+        open={this.state.open}
+        onClicked={this.onClicked} />
     }
 
     if(this.props.search_status){
@@ -29,27 +47,15 @@ var Authentication = React.createClass({
 })
 
 var LoginButton = React.createClass({
-    getInitialState: function(){
-    var state = {
-      open: false
-    }
-    return state
-  },
-
-  onClicked: function(){
-    this.setState({
-      open: !this.state.open
-    })
-  },
 
   render: function() {
-    var open = this.state.open ? "open" : "";
+    var open = this.props.open ? "open" : "";
     return (
       <div>
-        <div id="overlay" className={open} onClick={this.onClicked}></div>
+        <div id="overlay" className={open} onClick={this.props.onClicked}></div>
         <div id="mobile-nav">
-          <a href="#" className="menu-mobile-btn" onClick={this.onClicked}><img src="/img/menu.svg" /></a>
-          <div id="mobile-nav-content" className={open} onClick={this.onClicked}>
+          <a href="#" className="menu-mobile-btn" onClick={this.props.onClicked}><img src="/img/menu.svg" /></a>
+          <div id="mobile-nav-content" className={open} onClick={this.props.onClicked}>
             <NavLink href="/login"><FormattedMessage id="login" /></NavLink>
             <a href="/faq">FAQ</a>
           </div>
@@ -71,28 +77,14 @@ var LoginButton = React.createClass({
 
 var LogoutButton = React.createClass({
 
-
-  getInitialState: function(){
-    var state = {
-      open: false
-    }
-    return state
-  },
-
-  onClicked: function(){
-    this.setState({
-      open: !this.state.open
-    })
-  },
-
   render: function() {
-    var open = this.state.open ? "open" : "";
+    var open = this.props.open ? "open" : "";
     return (
       <div>
-        <div id="overlay" className={open} onClick={this.onClicked}></div>
+        <div id="overlay" className={open} onClick={this.props.onClicked}></div>
         <div id="mobile-nav">
-          <a href="#" className="menu-mobile-btn" onClick={this.onClicked} ><img src="/img/menu.svg" alias="Menu Button"/></a>
-          <div id="mobile-nav-content" className={open} onClick={this.onClicked}>
+          <a href="#" className="menu-mobile-btn" onClick={this.props.onClicked} ><img src="/img/menu.svg" alias="Menu Button"/></a>
+          <div id="mobile-nav-content" className={open} onClick={this.props.onClicked}>
             <NavLink href="/zadania"><FormattedMessage id="bank" /></NavLink>
             <NavLink href={'/wolontariusz/'+this.props.user_id}><FormattedMessage id="welcome" /> <b>{this.props.user_name}!</b></NavLink>
             <NavLink href="/ustawienia"><FormattedMessage id="settings" /></NavLink>
