f177154ddeb3988a925e198adfd4adc1b5d1710e
Janek <j.jaracz@poczta.pl>
2017-04-06 01:38:17 +0200

    Dogrywa pliki .js do landing page

---
+++ public/js/static-new/script.js
@@ -0,0 +1,41 @@
+/**
+ * Created by janek on 23.03.17.
+ */
+window.onload = function(){
+
+	let why = document.getElementById("why");
+	let nav = document.getElementById("nav");
+	let originClass = "main-nav ";
+	let newClass = "scrolled";
+	let navChange = false;
+	let target;
+
+	let toggleButtons = document.querySelectorAll(".toggle-button, #nav .list-element");
+	let openPane = false;
+	toggleButtons.forEach(btn => {
+		btn.addEventListener("click", () => {
+			nav.className = openPane ? nav.className.replace(/(?:^|\s)open(?!\S)/g, '') : nav.className + " open";
+			openPane = !openPane;
+			console.log("ClassName: ", nav.className);
+			console.log("OpenPane: ", openPane);
+		})
+	});
+
+	smoothScroll.init({
+		offset: nav.offsetHeight < 100 ? 50 : 0
+	});
+	gumshoe.init({
+		offset: nav.offsetHeight < 100 ? 50 : 0
+	});
+
+	window.addEventListener("scroll", ()=>{
+		target = gumshoe.getCurrentNav().target;
+		if(target.id != "header" && !navChange){
+			nav.className = originClass + newClass;
+			navChange = !navChange;
+		}else if(target.id == "header" && navChange){
+			nav.className = originClass;
+			navChange = !navChange;
+		}
+	});
+};
